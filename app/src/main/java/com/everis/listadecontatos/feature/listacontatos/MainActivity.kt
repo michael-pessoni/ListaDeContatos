074e262e769e342a1a084452ad916db953468a2c
michael-pessoni <pessoni94@gmail.com>
2021-06-25 14:03:25 -0300

    Create HelperDB.kt

--- app/src/main/java/com/everis/listadecontatos/feature/listacontatos/MainActivity.kt
+++ app/src/main/java/com/everis/listadecontatos/feature/listacontatos/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.everis.listadecontatos.R
+import com.everis.listadecontatos.application.ContatoApplication
 import com.everis.listadecontatos.bases.BaseActivity
 import com.everis.listadecontatos.feature.contato.ContatoActivity
 import com.everis.listadecontatos.feature.listacontatos.adapter.ContatoAdapter
@@ -61,14 +62,11 @@ class MainActivity : BaseActivity() {
 
     private fun onClickBuscar(){
         val busca = etBuscar.text.toString()
-        var listaFiltrada: List<ContatosVO> = ContatoSingleton.lista
-        if(!busca.isNullOrEmpty()){
-            listaFiltrada = ContatoSingleton.lista.filter { contato ->
-                if (contato.nome.toLowerCase().contains(busca.toLowerCase())){
-                    return@filter true
-                }
-                return@filter false
-            }
+        var listaFiltrada: List<ContatosVO> = mutableListOf()
+        try {
+            listaFiltrada = ContatoApplication.instance.helperDB?.buscarContatos(busca) ?: mutableListOf()
+        }catch (ex: Exception){
+            ex.printStackTrace()
         }
         adapter = ContatoAdapter(this,listaFiltrada) {onClickItemRecyclerView(it)}
         recyclerView.adapter = adapter
@@ -76,3 +74,5 @@ class MainActivity : BaseActivity() {
     }
 
 }
+
+
